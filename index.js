c07c3e4bc697f10671c0d60d688a39b0fbbd58f7
shubham-root <shubham.root@gmail.com>
2021-04-08 14:53:58 +0530

    notarizing requirements

--- index.js
+++ index.js
@@ -90,6 +90,8 @@ const runAction = () => {
 	if (platform === "mac") {
 		setEnv("CSC_LINK", getInput("mac_certs"));
 		setEnv("CSC_KEY_PASSWORD", getInput("mac_certs_password"));
+		setEnv("APPLEID". getInput("appleid", true));
+		setEnv("APPLEIDPASS", getInput("appleidpass", true));
 	} else if (platform === "windows") {
 		setEnv("CSC_LINK", getInput("windows_certs"));
 		setEnv("CSC_KEY_PASSWORD", getInput("windows_certs_password"));
